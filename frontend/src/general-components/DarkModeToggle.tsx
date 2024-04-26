82c9ff5151b30747a1c746dfa0416c1a56e85fef
50465 - Joao Cardoso <joaocardoson1@icloud.com>
2024-04-26 10:12:12 +0100

    Updated 404 Updated darkmode and lightmode styles Added icons for dashboard

--- frontend/src/general-components/DarkModeToggle.tsx
+++ frontend/src/general-components/DarkModeToggle.tsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
 import { DarkModeContext} from "../App";
+import SunIcon from '../assets/sun.png';
+import MoonIcon from '../assets/moon.png';
 
 export function DarkModeToggle() {
     const { isDark, toggleDarkMode } = useContext(DarkModeContext);
 
+    const sunImg = <img src={SunIcon} alt="sun" className="w-6 h-6" />;
+    const moonImg = <img src={MoonIcon} alt="moon" className="w-6 h-6" />;
+
     return (
         <button onClick={toggleDarkMode} className={"flex w-9 h-9 justify-center text-center items-center rounded-3xl dark-gray-shadow dark:blue-shadow dark:bg-darkComponentBg-600"}>
-            {isDark ? '☀️' : '🌙'}
+            {isDark ? sunImg : moonImg}
         </button>
     );
 }
--- frontend/src/pages/404/NotFound.tsx
+++ frontend/src/pages/404/NotFound.tsx
@@ -1,13 +1,17 @@
 import {Link} from "react-router-dom";
+import {DarkModeToggle} from "../../general-components/DarkModeToggle";
 
 export const NotFound = () => {
     return (
             <div className={"flex flex-col h-screen w-screen justify-center align-middle items-center page-background dark:page-background"}>
+                <div className={"absolute top-0 right-0 z-50 m-2"}>
+                <DarkModeToggle/>
+                </div>
                 <div className={"flex flex-col items-center"}>
-                    <h1 className={"flex items-center text-center dark-sign-in-gradient bg-clip-text text-transparent font-semibold text-6xl mb-6 select-none"}>404 - Page Not Found</h1>
-                    <p className={"flex text-center text-white text-lg mt-6 mb-5 select-none"}>Unfortunately, the page you are looking for does not exist...</p>
-                    <p className={"flex text-white text-sm mt-3 select-none "}>
-                        Try heading back to the<Link  to="/" className={"flex ml-1 text-runeBlue-100 hover:text-runeBlue-700"}> landing page.</Link>
+                    <h1 className={"flex items-center text-center text-black dark:text-runeBlue-100 font-semibold text-6xl mb-6 select-none"}>404 - Page Not Found</h1>
+                    <p className={"flex text-center text-black dark:text-white text-lg mt-6 mb-5 select-none"}>Unfortunately, the page you are looking for does not exist...</p>
+                    <p className={"flex text-black dark:text-white text-sm mt-3 select-none "}>
+                        Try heading back to the<Link  to="/" className={"flex ml-1 text-runeBlue-700 dark:text-runeBlue-100 hover:text-runeBlue-700"}> landing page.</Link>
                     </p>
                 </div>
             </div>
